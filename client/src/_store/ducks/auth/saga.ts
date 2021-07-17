0b61e8a7fc8201f998fc3ddd8872e3fc42cf4b6a
bravecode <contact.bravecode@gmail.com>
2021-07-17 12:39:17 +0200

    Client - Request error handling

--- client/src/_store/ducks/auth/saga.ts
+++ client/src/_store/ducks/auth/saga.ts
@@ -1,27 +1,33 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { register } from '_services/auth.service';
 import { AxiosError, AxiosResponse } from 'axios';
-import { TypesAuthResponse, TypesErrorResponse, TypesRegisterDTO } from '_services/types';
-import { registerRequest, registerSuccess } from './actions';
-import { IAuthUser } from './reducer';
+import { TypesAuthResponse, TypesErrorResponse } from '_services/types';
+import { registerError, registerRequest, registerSuccess } from './actions';
 import { SagaIterator } from 'redux-saga';
 
-// eslint-disable-next-line require-yield
 function* handleRegisterRequest({ payload }: any): SagaIterator {
     try {
         const { username, email, password } = payload;
         
         const response: AxiosResponse<TypesAuthResponse> = yield call(register, username, email, password);
         
+        // Store access token
+        localStorage.setItem('token', response.data.auth.token);
+
+        // Store user data
         put(registerSuccess({
             ID: response.data.user.id,
             email: response.data.user.email,
             username: response.data.user.username,
         }));
-
     } catch (err) {
-        console.log('error!!!');
-        console.log(err);
+        const typed: AxiosError<TypesErrorResponse> = err;
+        
+        if (typed.response?.data.errors) {
+            put(registerError(typed.response.data.errors))  
+        } else {
+            put(registerError(['Unknown error has occured.']))  
+        }
     }
 }
 
